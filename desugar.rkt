3936cebfdc5ba296950357ebd512d62e797e7693
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-03-11 14:45:41 -0500

    Start a desugaring

--- desugar.rkt
+++ desugar.rkt
@@ -2,36 +2,192 @@
 
 ;; Defines the desugaring from the surface syntax to the core syntax
 
-(provide desugar-actor)
+;; (provide desugar-program)
 
 (require
- redex/reduction-semantics
- "csa.rkt")
+ ;; redex/reduction-semantics
+ ;; "csa.rkt"
+ )
 
 ;; TODO: consider using Nanopass for this transformation
 
 ;; Translates an actor definition from the surface syntax into the core language syntax
 ;;
 ;; TODO: add actor args
-(define (desugar-actor actor-def address)
-  (term (desugar-actor/mf ,actor-def ,address))
-  )
+;; (define (desugar-actor actor-def address)
+;;   (term (desugar-actor/mf ,actor-def ,address))
+;;   )
 
-(define-metafunction csa-eval
-  ;; TODO: define some sort of Redex language that defines the surface-level programs
-  desugar-actor/mf : any a -> αn
-  [(desugar-actor/mf (define-actor (_) e S ...) a)
-   (a ((S ...) e))])
+;; (define-metafunction csa-eval
+;;   ;; TODO: define some sort of Redex language that defines the surface-level programs
+;;   desugar-actor/mf : any a -> αn
+;;   [(desugar-actor/mf (define-actor (_) e S ...) a)
+;;    (a ((S ...) e))])
 
-(module+ test
-  (require rackunit)
+;; (module+ test
+;;   (require rackunit)
 
-  (check-equal?
-   (desugar-actor
-    (term (define-actor (IgnoreAll)
-            (goto Always)
-            (define-state (Always) (m) (goto Always))))
-    (term (addr 0)))
-   (term ((addr 0)
-          (((define-state (Always) (m) (goto Always)))
-           (goto Always))))))
+;;   (check-equal?
+;;    (desugar-actor
+;;     (term (define-actor (IgnoreAll)
+;;             (goto Always)
+;;             (define-state (Always) (m) (goto Always))))
+;;     (term (addr 0)))
+;;    (term ((addr 0)
+;;           (((define-state (Always) (m) (goto Always)))
+;;            (goto Always))))))
+
+;; ---------------------------------------------------------------------------------------------------
+
+;; (define desugar-program
+;;   (compose )
+;;   (unparse-csa/actors-inlined
+;;    (desugar-pass
+;;     (with-output-language (csa/surface ActorDef)
+;;       `,a))))
+
+;; (define desugar-program
+;;   (compose
+;;    unparse-csa/inlined-actors
+;;    inline-actor-defs
+;;    inline-functions
+;;    parse-csa/surface))
+
+;; ---------------------------------------------------------------------------------------------------
+
+(require nanopass)
+
+(define (name? x) (and (symbol? x) (not (PrimOp? x))))
+(define (PrimOp? x) (not (not (member x (list '+ '-)))))
+
+(check-not-false (PrimOp? '+))
+(check-false (name? '+))
+(check-false (PrimOp? 'x))
+(check-true (name? 'x))
+
+(define-language csa/surface
+  (terminals
+   (number (n))
+   (boolean (b))
+   (name (x f a s))
+   ;; (PrimOp (po))
+   )
+  ;; (Prog (P)
+  ;;   (ad ... spawn-exp))
+  (ActorDef (ad)
+    (define-actor (a x ...) (fd ...) S ... e))
+  (StateDef (S)
+    (define-state (s x ...) (x2) e))
+  (Exp (e body)
+       n
+       b
+       x
+       (goto s e ...)
+       (send e1 e2)
+       spawn-exp
+       (begin e1 e* ...)
+       (f e ...)
+       ;; (po e ...)
+       (+ e ...)
+       (- e ...)
+       (let (lb ...) e2)
+       (let* (lb ...) e2))
+  (SpawnExp (spawn-exp)
+            (spawn a e ...))
+  (LetBinding (lb)
+              [x e])
+  (FuncDef (fd)
+           (define-function (f x ...) e)))
+
+;; (define (parse-csa/surface prog)
+;;   (with-output-language (csa/surface Prog)
+;;     `,prog))
+
+;; TODO: how does Nanopass resolve ambiguity?
+
+;; (define-pass
+;;   let*->let : L1 (P) -> L1
+;;   (Exp (e) : Exp -> Exp
+;;        [(let* () ,[e]) ,e]
+;;        [(let* ([,x1 ,e1] [x* e*] ...))
+;;         (let ([x1 e1]) (let* ([x* e*] ...)))
+;;         ]
+;;        )
+
+;;   )
+
+;; (module+ test
+;;   (let*->let
+;;    `(let* ([a 1] [b 2] [c 3]) (+ a b c)))
+;;   `(let ([a 1])
+;;      (let ([b 2])
+;;        (let ([c 3])))))
+
+;; ---------------------------------------------------------------------------------------------------
+;; Function inlining
+
+(define-language csa/inlined-funcs
+  (extends csa/surface)
+  (ActorDef (ad)
+            (- (define-actor (a x ...) (fd ...) S ... e))
+            (+ (define-actor (a x ...)          S ... e)))
+  (Exp (e) (- (f e ...))))
+
+(struct func-record (name formals body))
+
+(define-pass inline-functions : csa/surface (actor-def) -> csa/inlined-funcs ()
+  (definitions
+    (define funcs null))
+  (ActorDef : ActorDef (d) -> ActorDef ()
+    [(define-actor (,a ,x1 ...) ((define-function (,f ,x2 ...) ,[body]) ,fd* ...) ,S ... ,e)
+     (set! funcs (cons (func-record f x2 body) funcs))
+     (ActorDef
+      (with-output-language (csa/surface ActorDef)
+        `(define-actor (,a ,x1 ...) (,fd* ...) ,S ... ,e)))]
+    [(define-actor (,a ,x ...) () ,[S] ... ,[e])
+     `(define-actor (,a ,x ...) ,S ... ,e)])
+  (Exp : Exp (e) -> Exp ()
+       ;; TODO: see tmp/expected-meta for why this breaks
+
+    [(,f ,[e*] ...)
+         (define (name-matches? rec) (eq? f (func-record-name rec)))
+         (match (findf name-matches? funcs)
+           [#f (error 'inline-functions "could not find match for function ~s\n") f]
+           [(func-record _ (list formals ...) body)
+            `(let ([,formals ,e*] ...) ,body)])]
+        [(,po ,[e*] ...)
+     (,po ,e* ...)])
+  (StateDef : StateDef (S) -> StateDef ()
+            [(define-state (,s ,x1 ...) (,x2) ,[e])
+             `(define-state (,s ,x1 ...) (,x2) ,e)]))
+
+(require rackunit)
+
+(check-equal?
+ (unparse-csa/inlined-funcs
+  (inline-functions
+   (with-output-language (csa/surface ActorDef)
+     `(define-actor (A)
+        (     (define-function (foo x) (+ x 2))
+              (define-function (bar x y) (- x y)))
+        (foo (bar 3 4))))))
+ `(define-actor (A)
+    (let ([x
+           (let ([x 3]
+                 [y 4])
+             (- x y))])
+      (+ x 2))))
+
+;; ---------------------------------------------------------------------------------------------------
+
+;; (define-language csa/inlined-actors
+;;   (extends csa/inlined-funcs)
+;;   (Prog (P)
+;;         (- (ad ... spawn-exp))
+;;     (+ spawn-exp))
+;;   (SpawnExp (spawn-exp)
+;;             (- (spawn a e ...))
+;;             (+ (spawn e S ...))))
+
+;; (define (inline-actor-defs)
+;;   (error "not implemented"))
