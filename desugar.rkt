3405c3d0886e62e0fc6549da33b74895d4744da0
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-03-25 10:40:36 -0400

    Remove old comments

--- desugar.rkt
+++ desugar.rkt
@@ -4,37 +4,6 @@
 
 (provide desugar-single-actor-program)
 
-(require
- "csa.rkt")
-
-;; TODO: consider using Nanopass for this transformation
-
-;; Translates an actor definition from the surface syntax into the core language syntax
-;;
-;; TODO: add actor args
-;; (define (desugar-actor actor-def address)
-;;   (term (desugar-actor/mf ,actor-def ,address))
-;;   )
-
-;; (define-metafunction csa-eval
-;;   ;; TODO: define some sort of Redex language that defines the surface-level programs
-;;   desugar-actor/mf : any a -> αn
-;;   [(desugar-actor/mf (define-actor (_) e S ...) a)
-;;    (a ((S ...) e))])
-
-;; (module+ test
-;;   (require rackunit)
-
-;;   (check-equal?
-;;    (desugar-actor
-;;     (term (define-actor (IgnoreAll)
-;;             (goto Always)
-;;             (define-state (Always) (m) (goto Always))))
-;;     (term (addr 0)))
-;;    (term ((addr 0)
-;;           (((define-state (Always) (m) (goto Always)))
-;;            (goto Always))))))
-
 ;; ---------------------------------------------------------------------------------------------------
 
 (require nanopass)
