1c45a089844d777c851a2f35405eea0b12b5c125
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-04-09 13:51:15 -0400

    AuthN example fully written (untested)

--- authenticated-session.rkt
+++ authenticated-session.rkt
@@ -19,11 +19,11 @@
 ;; match actor ID?). Maybe just use ints, with the understanding that someone without a session
 ;; provides the special session ID 0
 
-(define-variant GetSession
+(define-variant GetSessionType
  (GetSession [id Nat] [reply-to (Addr GetSessionResult)]))
 
 (define-variant GetSessionResult
-  (ActiveSession [service (Addr SessionCommand)])
+  (ActiveSession [session-id Nat] [service (Addr SessionCommand)])
   (NewSession [auth (Addr Authenticate)]))
 
 (define-variant SessionCommand
@@ -38,8 +38,11 @@
   [reply-to (Addr AuthenticateResult)])
 
 (define-variant AuthenticateResult
-  (ActiveSession [service (Addr SessionCommand)])
+  (ActiveSession [session-id Nat] [service (Addr SessionCommand)])
   (FailedSession))
+
+(define-variant GetSessionInternalType
+ (GetSessionInternal [id Nat] [reply-to (Addr GetSessionResultInternal)]))
 
 (define-variant GetSessionResultInternal
   (SuccessInternal)
@@ -51,29 +54,41 @@
 (define-variant CreateSessionResult
   (NewSessionInternal [session-id Nat]))
 
+(define-type HandshakeWorkerInput
+  (Union (SuccessInternal)
+         (FailureInternal)
+         (NewSessionInternal [session-id Nat])))
+
+(define-type ServerInput
+  (Union
+    (GetSessionInternal [id Nat] [reply-to (Addr GetSessionResultInternal)])
+    (CreateSession [username String] [reply-to (CreateSessionResult)])
+    (Ping [reply-to (Addr SessionResponse)])))
+
 ;; ---------------------------------------------------------------------------------------------------
 
-;; The service guard
-((Union
-  (GetSession [id Nat] [reply-to (Addr GetSessionResult)]))
- (define-state (Ready [auth (Addr ???)] [server (Addr ???)] [password-table (Hash String String)]) (m)
-   ;; can get GetSession, Succ/Fail auth result, CreateSession, GetAuth
-   (case m
-     [(GetSession session-id reply-to)
-      (spawn HandshakeWorker ???)
-      (goto Ready auth server password-table)
-      ]
-     ;; These next two shouldn't happen, so we ignore them
-     [(SuccessInternal client-service) (goto Ready auth server)]
-     [(FailureInternal) (goto Ready auth server)]))
+(define-actor
+  GetSessionType
+  (ServiceGuard [server (Addr (Union
+                               (Ping [reply-to (Addr SessionResponse)])
+                               (GetSessionInternal [id Nat]
+                                                   [reply-to (Addr GetSessionResultInternal)])
+                               SessionCommand))]
+                [password-table (Hash String String)])
+  ((Union
+    (GetSession [id Nat] [reply-to (Addr GetSessionResult)]))
+   (define-state (Ready) (m)
+     (case m
+       [(GetSession session-id reply-to)
+        (spawn HandshakeWorker session-id reply-to server password-table)
+        (goto Ready)]))
+   (goto Ready)))
 
- (goto Ready auth server))
-
-(define-actor (Union ???) (HandshakeWorker [session-id Nat]
-                                           [client (Addr GetSessionResult)]
-                                           [password-table (Hash String String)]
-                                           [auth (Addr ???)]
-                                           [server (Addr ???)])
+(define-actor HandshakeWorkerInput
+  (HandshakeWorker [session-id Nat]
+                   [client (Addr GetSessionResult)]
+                   [server (Addr ServerInput)]
+                   [password-table (Hash String String)])
   ;; TODO: try writing this in a different way where each stage gets a new actor, so as to avoid lots
   ;; of the "this should never happen" messages
 
@@ -82,8 +97,8 @@
   ;; for them was already used). Doesn't work in presence of dropped messages, though.
   (define-state (WaitingForMaybeSession) (m)
     (case m
-      [(SuccessInternal client-service)
-       (send client (ActiveSession client-service))
+      [(SuccessInternal)
+       (send client (ActiveSession session-id client-service))
        (goto Done)]
       [(FailureInternal)
        (send client (NewSession self))
@@ -107,27 +122,28 @@
              (send reply-to (Failure))
              (goto Done)])])]
       ;; These next three shouldn't happen at this point; ignore the message
-      [(SuccessInternal client-service) (goto WaitingForCredentials)]
+      [(SuccessInternal) (goto WaitingForCredentials)]
       [(FailureInternal) (goto WaitingForCredentials)]
       [(NewSessionInternal session-id) (goto WaitingForCredentials)]))
   (define-state (WaitingForServer) (m)
     (case m
       [(NewSessionInternal session-id client-service)
-       (send client (NewSession session-id client-service))
+       (send client (ActiveSession session-id client-service))
        (goto Done)]
-      [(SuccessInternal client-service) (goto WaitingForCredentials)]
-      [(FailureInternal) (goto WaitingForCredentials)]
-      [(NewSessionInternal session-id) (goto WaitingForCredentials)]))
+      ;; None of these should happen right now
+      [(SuccessInternal) (goto WaitingForServer)]
+      [(FailureInternal) (goto WaitingForServer)]
+      [(Authenticate u p r) (goto WaitingForServer)]))
   (define-state (Done) (m) (goto Done))
   ;; init:
   (begin
     ;; Hmm... this is a case where actors aren't a great choice, and you want something more
     ;; lightweight like sessions that more directly expresses the protocol/usage: just want to
-    ;; send/receive exactly once
+    ;; send/receive exactly once, for several messages in this sequence
     (send server (GetSessionInternal session-id self))
     (goto WaitingForMaybeSession)))
 
-(define-actor (Union ???) (Server ???)
+(define-actor ServerInput (Server)
   (define-state (Running [sessions (Hash Nat Nat)] [next-session-id Nat]) (m)
     (case m
       [(GetSessionInternal id reply-to)
@@ -144,4 +160,4 @@
       [(Ping reply-to)
        (send reply-to (Pong))
        (goto Running sessions next-session-id)]))
-  (goto Running))
+  (goto Running (hash) 0))
