57d8a4684aa55d6400883ee2bb7b7ff4943b8b2e
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-08-03 16:37:49 -0400

    Remove unused stuff from aps.rkt

--- aps.rkt
+++ aps.rkt
@@ -2,17 +2,14 @@
 
 ;; Concrete semantic domains for APS (specification language), and associated functions
 
-(provide aps
-         aps-eval
-         subst-n/aps
-         subst/aps
-         aps-valid-config?
-         aps-config-observable-addresses
-         aps-config-only-instance-address
+(provide
+ aps-eval
+ aps-valid-config?
+ aps-config-only-instance-address
 
-         ;; Testing helpers
-         make-spec
-         make-exclusive-spec)
+ ;; Testing helpers
+ make-spec
+ make-exclusive-spec)
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; APS
@@ -43,8 +40,6 @@
       (variant t po ...)
       (record [l po] ...)))
 
-;; TODO: remove the idea of instances, just need configs of a single instance
-
 (define-extended-language aps-eval
   aps
   (Σ ((z ...) (a ...) O))
@@ -53,38 +48,6 @@
   (σ a UNKNOWN)
   (u .... a)
   (v-hat a))
-
-(define-metafunction aps-eval
-  subst-n/aps : e-hat (x v-hat) ... -> e-hat
-  [(subst-n/aps e-hat) e-hat]
-  [(subst-n/aps e-hat (x v-hat) any_rest ...)
-   (subst-n/aps (subst/aps e-hat x v-hat) any_rest ...)])
-
-(define-metafunction aps-eval
-  subst/aps : e-hat x v-hat -> e-hat
-  [(subst/aps (goto s u ...) x v-hat)
-   (goto s (subst/aps/u u x v-hat) ...)]
-  [(subst/aps (with-outputs ([u po] ...) e-hat) x v-hat)
-   (with-outputs ([(subst/aps/u u x v-hat) (subst/aps/po po x v-hat)] ...) (subst/aps e-hat x v-hat))]
-  ;; TODO: write the other clauses
-  )
-
-(define-metafunction aps-eval
-  subst/aps/u : u x v-hat -> u
-  [(subst/aps/u x x v-hat) v-hat]
-  [(subst/aps/u x_2 x v-hat) x_2]
-  [(subst/aps/u a x v-hat) a])
-
-(define-metafunction aps-eval
-  subst/aps/po : po x v-hat -> po
-  [(subst/aps/po x x v-hat) v-hat]
-  [(subst/aps/po x_2 x v-hat) x_2]
-  [(subst/aps/po a x v-hat) a]
-  [(subst/aps/po t x v-hat) t]
-  [(subst/aps/po * x v-hat) *]
-  [(subst/aps/po (variant t po ...) x v-hat) (variant t (subst/aps/po po x v-hat) ...)]
-  [(subst/aps/po (record [l po] ...) x v-hat)
-   (record [l (subst/aps/po x v-hat)] ...)])
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; Predicates
@@ -107,26 +70,7 @@
     (check-equal? (aps-config-only-instance-address spec) (term (addr 2 Nat)))))
 
 ;; ---------------------------------------------------------------------------------------------------
-;; Misc.
-
-(define (aps-config-observable-addresses config)
-  (term (config-observable-addresses/mf ,config)))
-
-(define-metafunction aps-eval
-  config-observable-addresses/mf : Σ -> (a ...)
-  [(config-observable-addresses/mf ((z ...) _ _))
-   (a ... ...)
-   (where ((_ (goto s a ...) _) ...) (z ...))])
-
-(module+ test
-  (check-equal?
-   (aps-config-observable-addresses
-    (make-exclusive-spec
-     (term (((define-state (Always r1 r2)
-               [* -> (goto Always r1 r2)]))
-            (goto Always (addr 3 Nat) (addr 4 Nat))
-            (addr 1 Nat)))))
-   (term ((addr 3 Nat) (addr 4 Nat)))))
+;; Testing Helpers
 
 (define (make-exclusive-spec instance)
   (make-spec instance null))
