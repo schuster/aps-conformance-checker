9f088ee4221bfea777e2fc8df8652bd3e9a54384
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-09-16 12:04:00 -0400

    Updates to new log analysis

--- new-log-analysis.rkt
+++ new-log-analysis.rkt
@@ -5,7 +5,7 @@
  "checker-data-structures.rkt"
  "main.rkt")
 
-(define initial-pairs-set (mutable-set))
+(define initial-pairs null)
 (define exploring (mutable-set))
 (define related-pairs (mutable-set))
 (define unrelated-pairs (mutable-set))
@@ -14,17 +14,25 @@
 
 (displayln "Reading the file")
 
+(printf "Time 1: ~s\n" (current-seconds))
+
+(define most-recent-pair #f)
+(define pairs-so-far 0)
+
 ;; read in the file
-(call-with-input-file "checker_run_log.dat"
+(call-with-input-file "examples/checker_run_log.dat"
   (lambda (file)
     (let loop ()
       (match (fasl->s-exp file)
         [(? eof-object?) (void)]
         [(list 'initial-pair pair)
-         (set-add! initial-pairs-set pair)
+         (set! initial-pairs (cons pair initial-pairs))
          (loop)]
         [(list 'exploring pair)
          (set-add! exploring pair)
+         (set! pairs-so-far (add1 pairs-so-far))
+         (printf "read number ~s\n" pairs-so-far)
+         (set! most-recent-pair pair)
          (loop)]
         [(list 'related-spec-step config-and-i-step related-steps)
          (hash-set! related-spec-steps config-and-i-step (list->set related-steps))
@@ -43,6 +51,9 @@
             (set-add! the-set step)])
          (loop)]))))
 
+(printf "Time 2: ~s\n" (current-seconds))
+(displayln "Read the whole file")
+
 ;; This is a copy of the remaining section of check-conformance/config, so that I can re-run the rest
 ;; of the algorithm on the logged data
 (define (check initial-pairs
@@ -50,20 +61,35 @@
                rank1-unrelated-successors
                incoming-steps
                rank1-related-spec-steps)
+  (printf "Current time: ~s\n" (current-seconds))
+  (printf "Num initial pairs: ~s\n" (length initial-pairs))
+  (printf "Num related r1: ~s\n" (set-count rank1-pairs))
+  (printf "Num unrelated r1: ~s\n" (set-count rank1-unrelated-successors))
+  (printf "All in r1: ~s\n" (andmap (curry set-member? rank1-pairs) initial-pairs))
   (match-define (list simulation-pairs simulation-related-spec-steps)
     (prune-unsupported rank1-pairs
                        incoming-steps
                        rank1-related-spec-steps
                        rank1-unrelated-successors))
+  (printf "Current time: ~s\n" (current-seconds))
+  (printf "Num related simulation: ~s\n" (set-count simulation-pairs))
+  (printf "All in simulation: ~s\n" (andmap (curry set-member? simulation-pairs) initial-pairs))
   (match-define (list commitment-satisfying-pairs unsatisfying-pairs)
     (partition-by-satisfaction simulation-pairs incoming-steps simulation-related-spec-steps))
+  (printf "Current time: ~s\n" (current-seconds))
+  (printf "Num com-sat: ~s\n" (set-count commitment-satisfying-pairs))
+  (printf "Num com-unsat: ~s\n" (set-count unsatisfying-pairs))
+  (printf "All in com-sat: ~s\n" (andmap (curry set-member? commitment-satisfying-pairs) initial-pairs))
   (match-define (list conforming-pairs _)
     (prune-unsupported commitment-satisfying-pairs
                        incoming-steps
                        simulation-related-spec-steps
                        unsatisfying-pairs))
+  (printf "Current time: ~s\n" (current-seconds))
+  (printf "Num conforming: ~s\n" (set-count conforming-pairs))
+  (printf "All in conforming: ~s\n" (andmap (curry set-member? conforming-pairs) initial-pairs))
   (andmap (curry set-member? conforming-pairs) initial-pairs))
 
 (displayln "Checking conformance")
 
-(check (set->list initial-pairs-set) related-pairs unrelated-pairs incoming related-spec-steps)
+(check initial-pairs related-pairs unrelated-pairs incoming related-spec-steps)
