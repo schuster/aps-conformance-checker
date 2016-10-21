a46b04c346198874bcc672b9c6ac82d173254fde
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2016-10-21 17:09:43 -0400

    More printing inside state explorer

--- state-explorer.rkt
+++ state-explorer.rkt
@@ -7,11 +7,18 @@
  "main.rkt")
 
 (match-define (config-pair i s)
-  (call-with-input-file "unrelated_6oct.rktd"
+  (call-with-input-file "auth_debug/maybe_before_unrelated.rktd"
     (lambda (file)
       (read file))))
 
 (define the-impl-steps (impl-steps-from i s))
 (for ([i-step the-impl-steps])
   (displayln (impl-step-trigger i-step))
-  (printf "Matching spec steps: ~s\n" (set-count (matching-spec-steps s i-step))))
+
+  (printf "Number of matching spec steps: ~s\n" (set-count (matching-spec-steps s i-step)))
+  (for ([matching-step (matching-spec-steps s i-step)])
+    (printf "config pair: ~s\n"
+            (pair->debug-pair (config-pair (impl-step-destination i-step)
+                                           (spec-step-destination matching-step))))
+    ;; (printf "~s\n\n" matching-step)
+    ))
