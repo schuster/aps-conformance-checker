c9a8e9090cc4408a48a497e158f5c7108bddd69a
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2017-01-04 12:15:09 -0500

    Finish pre-testing code for chat room

--- examples/chat.rkt
+++ examples/chat.rkt
@@ -10,14 +10,44 @@
 
 (program (receptionists [auth AuthCommand]) (externals)
   (define-constant pw-table (hash ["joe" "abc"] ["sally" "xyz"]))
+
+  (define-type RoomCommand
+    (Union
+     (Speak String String)
+     (Leave String)
+     (GetMembers (Addr (Listof String)))))
+
+  (define-variant RoomEvent
+    (JoinedRoom [room (Addr RoomCommand)])
+    (MemberLeft [username String])
+    (MemberJoined [username String])
+    (Message [username String] [message String]))
+
+  (define-type ServerCommand
+    (Union
+     (JoinRoom String String (Addr RoomEvent))
+     (GetRoomList (Addr (Listof String)))))
+
   (define-variant LogInResponse
     (AuthenticationFailed)
     (AuthenticationSucceeded [server (Addr ServerCommand)]))
+
   (define-variant AuthCommand
     (LogIn [username String] [password String] [callback (Addr LogInResponse)]))
 
+  (define-variant ChatServerInput
+    (JoinRoom [room-name String] [username String] [user (Addr RoomEvent)])
+    (GetRoomList [callback (Addr (Listof String))])
+    (RoomClosed [name String]))
+
+  (define-variant ChatRoomInput
+    (UserJoining [name String] [address (Addr RoomEvent)])
+    (Speak [name String] [message String])
+    (Leave [name String])
+    (GetMembers [callback (Addr (Listof String))]))
+
   (define-actor AuthCommand
-    Authenticator
+    (Authenticator [pw-table (Hash String String)] [server (Addr ServerCommand)])
     ()
     (goto Always)
     (define-state (Always) (m)
@@ -35,50 +65,39 @@
                (send callback (AuthenticationFailed))])
             (goto Always)])])))
 
-  (define-variant RoomEvent
-    [(JoinedRoom (Addr (Union [Speak String])))]
-    [(Message )])
-  (define-variant ChatServerInput
-    ;; TODO: join room, get room list, get room-done notification
-    [JoinRoom [name String] [user (Addr RoomEvent)]]
-    [GetRoomList [callback (Addr (Listof String))]])
-
-  ;; room commands: speak, leave, get members
-
-  ;; TODO: what about prefixing names onto messages? Without address comparison, this is hard
-
-  ;; TODO: hmm, lack of "hash-keys" makes returning a room list tricky, but adding hash-keys would
-  ;; change a *lot* of my abstract interpretation
-
-  (define-variant ChatRoomInput
-    (UserJoining [name String] [address (Addr RoomEvent)])
-    (Speak [name String] [message String])
-    (Leave [name String])
-    (GetMembers [callback (Addr (Listof String))]))
-
   (define-actor ChatRoomInput
     (ChatRoom [name String] [server (Addr (Union (RoomClosed String)))])
     ()
-    (goto Running (list))
+    (goto Running (hash))
     (define-state (Running [users (Hash String (Addr RoomEvent))]) (m)
       (case m
         [(UserJoining username user)
          (send user (JoinedRoom self))
-         (goto Running (cons user users))]
-        [(Speak message)
          (for/fold ([dummy 0])
-                   ([user users])
-           (send user (Message message))
+                   ([other-user (hash-values users)])
+           (send other-user (MemberJoined username))
+           0)
+         (goto Running (hash-set users username user))]
+        [(Speak speaker-name message)
+         (for/fold ([dummy 0])
+                   ([user (hash-values users)])
+           (send user (Message speaker-name message))
            0)
          (goto Running users)]
         [(Leave username)
          (cond
            [(hash-has-key? users username)
-            (cond
-              [(hash-empty? users)
-               (send server (RoomClosed name))
-               (goto Done)]
-              [else (goto Running (hash-remove users))])]
+            (let ([remaining-users (hash-remove users username)])
+              (cond
+                [(hash-empty? remaining-users)
+                 (send server (RoomClosed name))
+                 (goto Done)]
+                [else
+                 (for/fold ([dummy 0])
+                           ([remaining-user (hash-values remaining-users)])
+                   (send remaining-user (MemberLeft username))
+                   0)
+                 (goto Running remaining-users)]))]
            [else (goto Running users)])]
         [(GetMembers callback)
          (send callback (hash-keys users))
@@ -91,13 +110,15 @@
     (goto Always (hash))
     (define-state (Always [rooms (Hash String (Addr RoomCommand))]) (m)
       (case m
-        [(JoinRoom room-name user)
+        [(JoinRoom room-name username user)
          (case (hash-ref rooms room-name)
            [(Nothing)
-            (let ([new-room (spawn new-room Room name)])
-              (send new-room (UserJoining user)))]
+            (let ([new-room (spawn new-room ChatRoom name self)])
+              (send new-room (UserJoining username user))
+              (goto Always (hash-set rooms room-name new-room)))]
            [(Just room)
-            (send new-room (UserJoining user))])]
+            (send new-room (UserJoining username user))
+            (goto Always rooms)])]
         [(GetRoomList callback)
          (send callback (hash-keys rooms))
          (goto Always rooms)]
@@ -107,8 +128,8 @@
            [(Just r)
             (goto Always (hash-remove rooms room-name))])])))
 
-  (actors  [server (spawn server ChatServer)]
-           [auth (spawn auth Authenticator pw-table server)]))))
+  (actors [server (spawn server ChatServer)]
+          [auth (spawn auth Authenticator pw-table server)]))))
 
 (module+ test
   (require
