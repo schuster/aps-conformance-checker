d8d8e649613ba8ed30ca7af6836c30d2180fd95b
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2017-01-11 09:13:18 -0500

    Add Java analyzer to Ensime

--- examples/ensime.rkt
+++ examples/ensime.rkt
@@ -8,7 +8,7 @@
 ;; My version here simply stubs out the actual implementations for each of the RPC-like calls (which
 ;; largely involve no communication internally), focusing on the communication topology between the
 ;; Project actor and its children. The Project has 5 children (actors for the doc resolver, the
-;; indexer, the debugger, the Java compiler, and the Scala compiler), and each incoming RPC is simply
+;; indexer, the debugger, the Java analyzer, and the Scala analyzer), and each incoming RPC is simply
 ;; dispatched to the appropriate actor.
 ;;
 ;; This example shows how my conformance checker can deal with simple RPC calls even when they involve
@@ -17,6 +17,8 @@
 (define ensime-program
   (quasiquote
 (program (receptionists [project ProjectActorInput]) (externals)
+
+(define-type Boolean (Union [True] [False]))
 
 (define-variant ConnectionInfo
   (ConnectionInfo))
@@ -104,6 +106,33 @@
          [(Just val) (send sender (DebugValue val))])
        (goto Always)])))
 
+; stubbing out types for File and FileLocation
+(define-type File (Hash Nat String))
+(define-type FileLocation Nat)
+(define-variant MaybeDocSigPair
+  (NoDocSigPair)
+  (DocSigPair [path String]))
+
+(define-type JavaAnalyzerInput
+  (Union
+   [DocUriAtPointReq File FileLocation (Addr MaybeDocSigPair)]
+   [CompletionsReq File Nat Nat Boolean Boolean (Addr (Listof String))]))
+
+(define-actor JavaAnalyzerInput
+  (JavaAnalyzer)
+  ()
+  (goto Always)
+  (define-state (Always) (m)
+    (case m
+      [(DocUriAtPointReq file loc sender)
+       (case (hash-ref file loc)
+         [(Nothing) (send sender (NoDocSigPair))]
+         [(Just path) (send sender (DocSigPair path))])
+       (goto Always)]
+      [(CompletionsReq file point max-results case-sensitive? reload? sender)
+       (send sender (list "a" "b" "c"))
+       (goto Always)])))
+
 (define-variant ProjectInput
   (ConnectionInfoReq [sender (Addr ConnectionInfo)])
   (Resolve [doc-sig-pair String] [sender (Addr ResolveResult)])
@@ -114,12 +143,20 @@
   (DebugRunReq [sender (Addr BooleanResponse)])
   (DebugStopReq [sender (Addr BooleanResponse)])
   (DebugValueReq [location DebugLocation]
-                 [sender (Addr (Union [DebugValue String] [FalseResponse]))]))
+                 [sender (Addr (Union [DebugValue String] [FalseResponse]))])
+  (DocUriAtPointReq [file File] [point FileLocation] [sender (Addr MaybeDocSigPair)])
+  (CompletionsReq [file File]
+                  [point FileLocation]
+                  [max-results Nat]
+                  [case-sensitive? Boolean]
+                  [reload? Boolean]
+                  [sender (Addr (Listof String))]))
 
 (define-actor ProjectInput
   (Project [docs (Addr DocResolverInput)]
            [indexer (Addr IndexerInput)]
            [debugger (Addr DebugActorInput)]
+           [javac (Addr JavaAnalyzerInput)]
    ;; TODO: put all the other actors here
    )
   ()
@@ -134,7 +171,9 @@
       [(TypeCompletionsReq q m s) (goto AwaitingConnectionInfoReq)]
       [(DebugRunReq s) (goto AwaitingConnectionInfoReq)]
       [(DebugStopReq s) (goto AwaitingConnectionInfoReq)]
-      [(DebugValueReq l s) (goto AwaitingConnectionInfoReq)]))
+      [(DebugValueReq l s) (goto AwaitingConnectionInfoReq)]
+      [(DocUriAtPointReq f p s) (goto AwaitingConnectionInfoReq)]
+      [(CompletionsReq f p m c r s) (goto AwaitingConnectionInfoReq)]))
   (define-state (HandleRequests) (m)
     (case m
       [(ConnectionInfoReq sender)
@@ -157,12 +196,19 @@
        (goto HandleRequests)]
       [(DebugValueReq l s)
        (send debugger (DebugValueReq l s))
+       (goto HandleRequests)]
+      [(DocUriAtPointReq f p s)
+       (send javac (DocUriAtPointReq f p s))
+       (goto HandleRequests)]
+      [(CompletionsReq f p m c r s)
+       (send javac (CompletionsReq f p m c r s))
        (goto HandleRequests)])))
 
 (actors [docs (spawn docs-loc DocResolver)]
         [indexer (spawn indexer-loc Indexer)]
         [debugger (spawn debugger-loc DebugActor)]
-        [project (spawn project-loc Project docs indexer debugger)]))))
+        [javac (spawn javac-loc JavaAnalyzer)]
+        [project (spawn project-loc Project docs indexer debugger javac)]))))
 
 ;; Messages to do:
 ;; * Javac: docUriAtPointReq, CompletionsReq
@@ -224,4 +270,20 @@
     (check-unicast value-dest (variant DebugValue "bar"))
     (define stop-dest (make-async-channel))
     (async-channel-put project (variant DebugStopReq stop-dest))
-    (check-unicast stop-dest (variant TrueResponse))))
+    (check-unicast stop-dest (variant TrueResponse))
+    ;; javac
+    (define javadoc-dest (make-async-channel))
+    (define test-file (hash 1 "a" 2 "b"))
+    (async-channel-put project (variant DocUriAtPointReq test-file 1 javadoc-dest))
+    (check-unicast javadoc-dest (variant DocSigPair "a"))
+    (async-channel-put project (variant DocUriAtPointReq test-file 3 javadoc-dest))
+    (check-unicast javadoc-dest (variant NoDocSigPair))
+    (define java-completions-dest (make-async-channel))
+    (async-channel-put project (variant CompletionsReq
+                                        test-file
+                                        5
+                                        10
+                                        (variant False)
+                                        (variant False)
+                                        java-completions-dest))
+    (check-unicast java-completions-dest (list "a" "b" "c"))))
